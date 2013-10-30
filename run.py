309f01e6e2893bbbca26769d946aad9f0cb35b5c
Maxime Petazzoni <max@signalfuse.com>
2013-10-29 19:21:40 -0700

    Style changes and comments in init script
    Signed-off-by: Maxime Petazzoni <max@signalfuse.com>

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ os.chdir(os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     '..'))
 
-CASSANDRA_CONFIG_FILE='conf/cassandra.yaml'
+CASSANDRA_CONFIG_FILE = 'conf/cassandra.yaml'
 
 # Environment variables driving the Cassandra configuration and their defaults.
 CASSANDRA_CONFIG_CLUSTER_NAME = os.environ.get('CASSANDRA_CONFIG_CLUSTER_NAME', 'Cassandra cluster')
@@ -20,9 +20,11 @@ CASSANDRA_CONFIG_LISTEN_ADDRESS = os.environ.get('CASSANDRA_CONFIG_LISTEN_ADDRES
 CASSANDRA_CONFIG_RPC_ADDRESS = os.environ.get('CASSANDRA_CONFIG_RPC_ADDRESS', '127.0.0.1')
 CASSANDRA_CONFIG_SEED_PEERS = os.environ.get('CASSANDRA_CONFIG_SEED_PEERS', '127.0.0.1')
 
+# Read and parse the existing file.
 with open(CASSANDRA_CONFIG_FILE) as f:
     conf = yaml.load(f)
 
+# Update the configuration settings we care about.
 conf.update({
     'cluster_name': CASSANDRA_CONFIG_CLUSTER_NAME,
     'data_file_directories': CASSANDRA_CONFIG_DATA_DIRS.split(','),
@@ -33,7 +35,9 @@ conf.update({
 
 conf['seed_provider'][0]['parameters'][0]['seeds'] = CASSANDRA_CONFIG_SEED_PEERS
 
+# Output the updated configuration.
 with open(CASSANDRA_CONFIG_FILE, 'w+') as f:
     yaml.dump(conf, f, default_flow_style=False)
 
+# Start Cassandra in the foreground.
 os.execl('bin/cassandra', 'cassandra', '-f')
