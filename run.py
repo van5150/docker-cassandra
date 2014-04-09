8ab81a942a83dc4055185036178d9b2717715927
Maxime Petazzoni <max@signalfuse.com>
2014-04-09 15:39:54 -0700

    Set visualvm.display.name in JVM_OPTS
    Signed-off-by: Maxime Petazzoni <max@signalfuse.com>

--- run.py
+++ run.py
@@ -62,6 +62,7 @@ os.environ['JVM_OPTS'] = ' '.join([
     '-javaagent:lib/jmxagent.jar',
     '-Dsf.jmxagent.port={}'.format(get_port('jmx', -1)),
     '-Djava.rmi.server.hostname={}'.format(get_container_host_address()),
+    '-Dvisualvm.display.name="{}/{}"'.format(get_environment_name(), get_container_name()),
     os.environ.get('JVM_OPTS', ''),
 ])
 
